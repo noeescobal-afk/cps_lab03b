b0a80205acb571a16b94d52583585091e8e89200
carlosandia-cpu <carlos.andia@tecsup.edu.pe>
2026-09-03 17:45:40 -0500

    Mantener metodo save en StudentRepository

--- src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
+++ src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
@@ -2,6 +2,10 @@ package pe.edu.tecsup.lab03.repositories;
 
 public class StudentRepository {
 
+    public void save(String name) {
+        System.out.println("Estudiante guardado en StudentRepository: " + name);
+    }
+
     public void update(String name) {
         System.out.println("Estudiante actualizado en StudentRepository: " + name);
     }
