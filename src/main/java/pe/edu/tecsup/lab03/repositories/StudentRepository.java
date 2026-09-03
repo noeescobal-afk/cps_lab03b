7070cbdc32f64ff8561a15c0b5bbcd54fed8a952
carlosandia-cpu <carlos.andia@tecsup.edu.pe>
2026-09-03 17:41:45 -0500

    Modificar StudentRepository en sprint-2

---
+++ src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
@@ -0,0 +1,8 @@
+package pe.edu.tecsup.lab03.repositories;
+
+public class StudentRepository {
+
+    public void update(String name) {
+        System.out.println("Estudiante actualizado en StudentRepository: " + name);
+    }
+}
