1b1729602e7bef2e77af14d38704efefa8c5ee9c
carlosandia-cpu <carlos.andia@tecsup.edu.pe>
2026-09-03 17:25:46 -0500

    Implementar metodo save en StudentRepository

--- src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
+++ src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
@@ -3,6 +3,6 @@ package pe.edu.tecsup.lab03.repositories;
 public class StudentRepository {
 
     public void save(String name) {
-        // Tu compañero del Repository pondrá la lógica aquí
+        System.out.println("Estudiante guardado en StudentRepository: " + name);
     }
 }
