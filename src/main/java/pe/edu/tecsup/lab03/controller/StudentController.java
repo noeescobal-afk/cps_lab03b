301aac8374ce116283c5e858c50d3258ab15fe1b
Ivan Jara Ayala <ivan.jara.a@tecsup.edu.pe>
2026-09-03 17:10:29 -0500

    Implementacion de StudentController

--- src/main/java/pe/edu/tecsup/lab03/controller/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controller/StudentController.java
@@ -1,4 +1,13 @@
 package pe.edu.tecsup.lab03.controller;
 
+import pe.edu.tecsup.lab03.service.StudentService;
+
 public class StudentController {
+
+    private final StudentService service = new StudentService();
+
+    public void registerStudent(String name) {
+        System.out.println("Procesando registro en StudentController para: " + name);
+        service.registerStudent(name);
+    }
 }
