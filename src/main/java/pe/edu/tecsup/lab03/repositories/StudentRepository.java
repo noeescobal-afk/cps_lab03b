c4f1fe6a381ffaa4b328713cbf71ae9df4897457
Carlos Andia <carlos.andia@tecsup.edu.pe>
2026-09-03 22:11:24 -0500

    Actualizar StudentRepository en sprint-2

--- src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
+++ src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
@@ -3,10 +3,7 @@ package pe.edu.tecsup.lab03.repositories;
 public class StudentRepository {
 
     public void save(String name) {
-        System.out.println("Estudiante guardado en StudentRepository: " + name);
-    }
-
-    public void update(String name) {
-        System.out.println("Estudiante actualizado en StudentRepository: " + name);
+        System.out.println("Guardando estudiante en StudentRepository: " + name);
+        System.out.println("Estudiante registrado correctamente.");
     }
 }
